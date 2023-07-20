08b436521e3d27e51cec3b337c41bc2ee7332616
lenblazy <lenblazy@gmail.com>
2023-07-20 10:57:33 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Lennox Mwabonje on 20/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
